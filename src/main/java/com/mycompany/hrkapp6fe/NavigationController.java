3b8b37815993c091ac08e46282db37c753599117
nleitefaria <nleitefaria@gmail.com>
2019-10-29 19:23:27 +0000

    INIT

--- src/main/java/com/mycompany/hrkapp6fe/NavigationController.java
+++ src/main/java/com/mycompany/hrkapp6fe/NavigationController.java
@@ -14,13 +14,4 @@ public class NavigationController {
 	public String listCustomers(){
 	    return "customers?faces-redirect=true"; 
 	}
-	
-	public String listCars(){
-	    return "cars?faces-redirect=true"; 
-	}
-	
-	public String listCustomers1(){
-	    return "customers1?faces-redirect=true"; 
-	}
-
 }
