1c591f92009087e6f95d270f417023730de42db5
nleitefaria <nleitefaria@nleitefaria-HP-EliteBook>
2019-11-01 00:22:30 +0000

    Added employees

--- src/main/java/com/mycompany/hrkapp6fe/bean/EmployeeBean.java
+++ src/main/java/com/mycompany/hrkapp6fe/bean/EmployeeBean.java
@@ -4,10 +4,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import org.primefaces.model.LazyDataModel;
-
-import com.mycompany.hrkapp6fe.dto.CustomersDTO;
 import com.mycompany.hrkapp6fe.dto.EmployeesDTO;
-import com.mycompany.hrkapp6fe.model.CustomerLazyDataModel;
 import com.mycompany.hrkapp6fe.model.EmployeeLazyDataModel;
 
 @ManagedBean
