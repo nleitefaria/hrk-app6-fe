154f68b7b9b116d50373010e36b9cf424b458fa5
nleitefaria <nleitefaria@gmail.com>
2019-11-09 21:43:11 +0000

    Update NavigationController.java

--- src/main/java/com/mycompany/hrkapp6fe/NavigationController.java
+++ src/main/java/com/mycompany/hrkapp6fe/NavigationController.java
@@ -8,7 +8,6 @@ import javax.faces.bean.RequestScoped;
 public class NavigationController {
 	
 	public String home(){
-		System.out.println("@home");
 	    return "index?faces-redirect=true";
 	}
 	
