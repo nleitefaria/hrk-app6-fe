79ab19e5744e7ee29bfe7519151535bd3b8ad803
nleitefaria <nleitefaria@nleitefaria-Latitude-5490>
2019-12-11 18:03:23 +0000

    Added select to Customers table

--- src/main/java/com/mycompany/hrkapp6fe/bean/CustomerBean.java
+++ src/main/java/com/mycompany/hrkapp6fe/bean/CustomerBean.java
@@ -2,18 +2,33 @@ package com.mycompany.hrkapp6fe.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+
 import org.primefaces.model.LazyDataModel;
 import com.mycompany.hrkapp6fe.dto.CustomersDTO;
 import com.mycompany.hrkapp6fe.model.CustomerLazyDataModel;
 
 @ManagedBean
-@RequestScoped
+//@RequestScoped
+@ViewScoped
 public class CustomerBean {
 	
 	CustomerLazyDataModel dataModel = new CustomerLazyDataModel();
 	
+	private CustomersDTO selectedCustomer;
+	
     public LazyDataModel<CustomersDTO> getModel(){
         return dataModel;
     }
+
+	public CustomersDTO getSelectedCustomer() {
+		return selectedCustomer;
+	}
+
+	public void setSelectedCustomer(CustomersDTO selectedCustomer) {
+		this.selectedCustomer = selectedCustomer;
+	}
+    
+    
 
 }
