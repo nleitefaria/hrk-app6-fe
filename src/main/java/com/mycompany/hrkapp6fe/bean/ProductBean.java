17e01149092a13f3d4eb780e8519459ff0ccba13
nleitefaria <nleitefaria@nleitefaria-Latitude-5490>
2019-12-17 16:55:00 +0000

    Updated products

--- src/main/java/com/mycompany/hrkapp6fe/bean/ProductBean.java
+++ src/main/java/com/mycompany/hrkapp6fe/bean/ProductBean.java
@@ -1,20 +1,28 @@
 package com.mycompany.hrkapp6fe.bean;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-
+import javax.faces.bean.ViewScoped;
 import org.primefaces.model.LazyDataModel;
-
 import com.mycompany.hrkapp6fe.dto.ProductsDTO;
 import com.mycompany.hrkapp6fe.model.ProductLazyDataModel;
 
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class ProductBean {
 
 	ProductLazyDataModel dataModel = new ProductLazyDataModel();
+	
+	private ProductsDTO selectedProduct;
 
     public LazyDataModel<ProductsDTO> getModel(){
         return dataModel;
     }
+
+	public ProductsDTO getSelectedProduct() {
+		return selectedProduct;
+	}
+
+	public void setSelectedProduct(ProductsDTO selectedProduct) {
+		this.selectedProduct = selectedProduct;
+	} 
 }
