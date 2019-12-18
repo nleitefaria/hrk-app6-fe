9f8a26b557f7840cbe36cf258e999cbfa0683fd8
nleitefaria <nleitefaria@nleitefaria-Latitude-5490>
2019-12-18 15:52:32 +0000

    Added Employee details

--- src/main/java/com/mycompany/hrkapp6fe/bean/EmployeeBean.java
+++ src/main/java/com/mycompany/hrkapp6fe/bean/EmployeeBean.java
@@ -1,20 +1,28 @@
 package com.mycompany.hrkapp6fe.bean;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-
+import javax.faces.bean.ViewScoped;
 import org.primefaces.model.LazyDataModel;
 import com.mycompany.hrkapp6fe.dto.EmployeesDTO;
 import com.mycompany.hrkapp6fe.model.EmployeeLazyDataModel;
 
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class EmployeeBean {
 	
 	EmployeeLazyDataModel dataModel = new EmployeeLazyDataModel();
+	
+	private EmployeesDTO selectedEmployee;
 
     public LazyDataModel<EmployeesDTO> getModel(){
         return dataModel;
     }
 
+	public EmployeesDTO getSelectedEmployee() {
+		return selectedEmployee;
+	}
+
+	public void setSelectedEmployee(EmployeesDTO selectedEmployee) {
+		this.selectedEmployee = selectedEmployee;
+	}
 }
